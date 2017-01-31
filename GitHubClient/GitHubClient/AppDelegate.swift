2f6b2a2a27c9eaab172e1fe1043ea3d22d1ea905
Fellipe Caetano <fellipe.caetano4@gmail.com>
2017-01-31 00:23:25 -0200

    Create Redux integration skeleton

--- GitHubClient/GitHubClient/AppDelegate.swift
+++ GitHubClient/GitHubClient/AppDelegate.swift
@@ -1,16 +1,22 @@
 import UIKit
+import Redux
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    let store = Store<AppState>(initialState: AppState(), reducer: AppReducer)
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.backgroundColor = .white
+
         self.window?.rootViewController = UINavigationController(
-            rootViewController: RepositoriesViewController(service: RepositoryService(username: "fellipecaetano"))
+            rootViewController: RepositoriesViewController(
+                service: RepositoryService(username: "fellipecaetano")
+            )
         )
+
         self.window?.makeKeyAndVisible()
         return true
     }
---
+++ GitHubClient/GitHubClient/Redux/AppRedux.swift
@@ -0,0 +1,7 @@
+import Redux
+
+struct AppState {}
+
+func AppReducer(state: AppState, action: Action) -> AppState {
+    return state
+}
